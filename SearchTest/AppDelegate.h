2b8a15c4214d303bb2a68fb9f4dbc8cfcd0f4a8c
Steven Thompson <steven.thompson.ca@gmail.com>
2015-10-30 21:50:26 -0400

    Initial Commit

---
+++ SearchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchTest
+//
+//  Created by steve on 2015-10-30.
+//  Copyright © 2015 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchTest
+//
+//  Created by steve on 2015-10-30.
+//  Copyright © 2015 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
